e91ad4073e9a87fa2a74528e45b01265f619e66a
Pmira01 <143371006+Pmira01@users.noreply.github.com>
2023-10-27 22:04:54 +0700

    Update sent.php

--- sent.php
+++ sent.php
@@ -1,54 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>Progate</title>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div class="header">
-    <div class="header-left">Progate</div>
-    <div class="header-right">
-      <ul>
-        <li>Tentang Progate</li>
-        <li>Pengerahan</li>
-        <li class="selected">Kontak</li>
-      </ul>
-    </div>
-  </div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Usia</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Mengambil data dari database atau sumber lainnya
+            $data = array(
+                array(1, 'John', 25),
+                array(2, 'Jane', 30),
+                array(3, 'Smith', 28),
+            );
 
-  <div class="main">
-    <div class="thanks-message">Terima kasih telah menghubungi kami!</div>
-    <div class="display-contact">
-      <div class="form-title">Terkirim</div>
-
-      <div class="form-item">■ Nama</div>
-      <?php echo $_POST['name']; ?>
-
-      <div class="form-item">■ Umur</div>
-      <?php echo $_POST['age']; ?>
-
-      <div class="form-item">■ Kategori</div>
-      <!-- Cetak kategori yang diterima dari formulir dibawah -->
-      <?php echo $_POST['category']; ?>
-
-      <div class="form-item">■ Pesan</div>
-      <?php echo $_POST['body']; ?>
-
-    </div>
-  </div>
-
-  <div class="footer">
-    <div class="footer-left">
-      <ul>
-        <li>Tentang Progate</li>
-        <li>Pengerahan</li>
-        <li>Kontak</li>
-      </ul>
-    </div>
-    <div class="like-box">
-      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
-    </div>
-  </div>
+            foreach ($data as $row) {
+                echo "<tr>";
+                echo "<td>" . $row[0] . "</td>";
+                echo "<td>" . $row[1] . "</td>";
+                echo "<td>" . $row[2] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
